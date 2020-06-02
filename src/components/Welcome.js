737c06d1ccfe9e5f52817765d86662ab7c15ed62
raul soissa <raulsoissa@gmail.com>
2020-06-02 16:39:48 -0400

    Video 14 Fucntional Component's

---
+++ src/components/Welcome.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './styles/Welcome.css'
+
+function Welcome(props) {
+    return (
+        <div className="container">
+            <div className="fitness-user-info">
+                <h1>Hello {props.username}!</h1>
+                <p>Let's workout to get someone gains!</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Welcome;
+
+// esto es un componente funcional, por lo que se declara una función en vez de una clase ya que este componente no utiliza el ciclo de vida del componente ni definir estado, como si ocurre en las clases
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './components/Card';
+import Welcome from './components/Welcome'
 
 const container = document.getElementById('root');
 
 
 ReactDOM.render(
-    <Card
-        title="Technique Guides"
-        description="Learn amazing street workout and calisthenics"
-        image="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
-        leftColor="#A74CF2"
-        rightColor="#617BFB"
-    />, container);
+    <div className="">
+        <Welcome
+            username="Raúl"
+        />
+        <Card
+            title="Technique Guides"
+            description="Learn amazing street workout and calisthenics"
+            image="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+            leftColor="#A74CF2"
+            rightColor="#617BFB"
+        />
+    </div>, container);
