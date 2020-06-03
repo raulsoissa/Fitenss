d9b25438b0d7b00a64fa4ba5bd1d0be53fab1b53
raul soissa <raulsoissa@gmail.com>
2020-06-02 21:51:42 -0400

    Video 21 in process, minute 5:00

--- src/pages/ExerciseNew.js
+++ src/pages/ExerciseNew.js
@@ -2,16 +2,50 @@ import React, { Component } from 'react'
 
 export default class ExerciseNew extends Component {
 
-    handleClick = () => {
-        console.log(this)
+    handleChange = (e) => {
+        console.log(`${e.target.name}: ${e.target.value}`)
     }
 
     render() {
         return (
-            <div>
-                <button onClick={this.handleClick}>
-                    Send
-                </button>
+            <div className="container">
+                <form action="">
+                    <div className="form-group">
+                        <input
+                            type="text" className="form-control" placeholder="title" 
+                            name="title" onChange={this.handleChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="text" className="form-control" placeholder="description"
+                            name="description" onChange={this.handleChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="text" className="form-control" placeholder="img"
+                            name="img" onChange={this.handleChange}
+                        />
+                    </div>
+                    <div className="form-row">
+                        <div className="col">
+                            <input
+                                type="text" className="form-control" placeholder="leftColor"
+                                name="leftColor" onChange={this.handleChange}
+                            />
+                        </div>
+                        <div className="col">
+                            <input
+                                type="text" className="form-control" placeholder="rightColor"
+                                name="rightColor" onChange={this.handleChange}
+                            />
+                        </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary">
+                        Submit
+                    </button>
+                </form>
             </div>
         )
     }
